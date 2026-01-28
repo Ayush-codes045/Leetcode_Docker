10a88108b4451aab4bd598217c0222b82fb315aa
Ayush <bittukumar8083a@gmail.com>
2026-01-28 10:38:50 +0530

    new commit

--- worker/program.cpp
+++ worker/program.cpp
@@ -1,10 +1,8 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a;
-    int b;
-    cin>>a;
-    cin>>b;
-    int sum=a+b;
-    cout<<sum;
+    int a,b;
+    cin>>a>>b;
+    int ans=a-b;
+    cout<<ans;
 }
